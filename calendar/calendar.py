6e18ddf85c6f58de913723a47e9b4b86ee936101
Damian Muszyński <muszynskidamian@wp.pl>
2016-05-30 23:17:27 +0200

    Update calendar.py

--- calendar/calendar.py
+++ calendar/calendar.py
@@ -1,4 +1,4 @@
-'''called with 2 argument except for the name of file itself
+'''called with 2 arguments except for the name of file itself
 eg: python my_calendar.py 2017 06
 (prints calendar for June, 2017)'''
 
